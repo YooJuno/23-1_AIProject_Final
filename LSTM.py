41fbfa0115e98d7b35f1d9b38f69e6780d3e6054
YooJuno <juno980220@naver.com>
2023-06-06 16:34:35 +0900

    LSTM으로 종가 예측하고, 변동성 돌파 전략을 매수가로 예측

--- LSTM.py
+++ LSTM.py
@@ -3,11 +3,17 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
+import datetime
+import schedule
+import time
+
 
 # 액세스 키 설정
 access_key = ""
 secret_key = ""
 upbit = pyupbit.Upbit(access_key, secret_key)
+REPL = "KRW-XRP"
+
 
 def get_historical_price(ticker, interval, count):
     try:
@@ -38,47 +44,115 @@ def create_sequences(data, seq_length):
         y.append(data[i+seq_length])
     return np.array(X), np.array(y)
 
-# 비트코인 가격 데이터 받아오기
-btc_data = get_historical_price("KRW-XRP", "minute1", 500)
-btc_prices = preprocess_data(btc_data)
+def get_target_price(ticker, k):
+    """변동성 돌파 전략으로 매수 목표가 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+    return target_price
+
+def get_start_time(ticker):
+    """시작 시간 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
+
+def get_current_price(ticker):
+    """현재가 조회"""
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
+
+predicted_close_price = 0
+def predict_price(ticker):
+    global predicted_close_price
+
+    # 비트코인 가격 데이터 받아오기
+    btc_data = get_historical_price(ticker, "minute1", 500)
+    btc_prices = preprocess_data(btc_data)
+
+    # 학습 데이터와 테스트 데이터 분리
+    train_size = int(len(btc_prices) * 0.8)
+    train_data = btc_prices[:train_size]
+    test_data = btc_prices[train_size:]
+
+    # 시퀀스 생성
+    seq_length = 10  # 입력 시퀀스 길이 설정
+    X_train, y_train = create_sequences(train_data, seq_length)
+    X_test, y_test = create_sequences(test_data, seq_length)
+
+    # 모델 구성
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.LSTM(128, return_sequences=True, input_shape=(seq_length, 1)),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.LSTM(64, return_sequences=True),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.LSTM(32),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(1)
+    ])
+    # 모델 컴파일
+    model.compile(optimizer='adam', loss='mse')
+    # 모델 학습
+    model.fit(X_train, y_train, epochs=50, batch_size=32, shuffle=False)
+    # 예측 수행
+    y_pred = model.predict(X_test)
+    # 스케일 역변환
+    y_pred = scaler.inverse_transform(y_pred)
+    y_test = scaler.inverse_transform(y_test)
+
+    # 예측 결과 출력
+    for i in range(len(y_pred)):
+        print("실제 가격: {:.2f}, 예측 가격: {:.2f}".format(y_test[i][0], y_pred[i][0]))
+
+    predicted_close_price = y_pred[i][-1]
+
+    print('predicted_close_price : ', predicted_close_price)
+
+def get_balance(ticker):
+    """잔고 조회"""
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
 
 
-# 학습 데이터와 테스트 데이터 분리
-train_size = int(len(btc_prices) * 0.8)
-train_data = btc_prices[:train_size]
-test_data = btc_prices[train_size:]
-
-# 시퀀스 생성
-seq_length = 10  # 입력 시퀀스 길이 설정
-X_train, y_train = create_sequences(train_data, seq_length)
-X_test, y_test = create_sequences(test_data, seq_length)
-
-# 모델 구성
-model = tf.keras.models.Sequential([
-    tf.keras.layers.LSTM(128, return_sequences=True, input_shape=(seq_length, 1)),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.LSTM(64, return_sequences=True),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.LSTM(32),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(1)
-])
+predict_price(REPL)
+schedule.every(30).minutes.do(lambda: predict_price(REPL))
 
 
 
-# 모델 컴파일
-model.compile(optimizer='adam', loss='mse')
+cnt = 0
+while True:
+    try:
+        print('\ncnt :',cnt)
+        cnt+=1
 
-# 모델 학습
-model.fit(X_train, y_train, epochs=50, batch_size=32, shuffle=False)
+        now = datetime.datetime.now()
+        start_time = get_start_time(REPL)
+        end_time = start_time + datetime.timedelta(days=1)
+        schedule.run_pending()
 
-# 예측 수행
-y_pred = model.predict(X_test)
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = get_target_price(REPL, 0.5)
+            print('target price :', target_price)
+            current_price = get_current_price(REPL)
+            print('current price :', current_price)
+            print('predicted_close_price : %.1f'%(predicted_close_price))
+            if target_price < current_price and current_price < predicted_close_price:
+                krw = get_balance("KRW")
+                print('krw :', krw)
+                if krw > 5000:
+                    # upbit.buy_market_order(REPL, krw*0.9995)
+                    print('juno buy!!!!!!!!!!')
+        else:
+            XRP = get_balance(REPL)
+            if XRP > 0.00008:
+                # upbit.sell_market_order(REPL, XRP*0.9995)
+                print('juno sell!!!!!!!!!!')
+        time.sleep(10)
+    except Exception as e:
+        print(e)
+        time.sleep(1)
 
-# 스케일 역변환
-y_pred = scaler.inverse_transform(y_pred)
-y_test = scaler.inverse_transform(y_test)
-
-# 예측 결과 출력
-for i in range(len(y_pred)):
-    print("실제 가격: {:.2f}, 예측 가격: {:.2f}".format(y_test[i][0], y_pred[i][0]))
